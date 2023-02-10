57efcd22180eb791ce5de663fce9cac9bffdd8bf
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2023-02-10 21:53:52 +0530

    Create 15 Indegree and OutDegree.c

---
+++ 15 Indegree and OutDegree.c
@@ -0,0 +1,53 @@
+// Indegree and Outdegree:
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int n, G[10][10], a[10];
+
+void readgraph()
+{
+    int i, j;
+    
+    printf("Enter the number of vertices of graph: ");
+    scanf("%d", &n);
+    
+    printf("Enter the adjacency matrix of graph:\n");
+    printf("  ");
+    for(i=0; i<n; i++) printf("%c ", i+97);
+    printf("\n");
+    
+    for(i=0; i<n; i++)
+    {
+        printf("%c ", i+97);
+        for(j=0; j<n; j++)
+            scanf("%d", &G[i][j]);
+    }
+}
+
+void in_outdegree()
+{
+    int i, j, s1, s2;
+    
+    for(i=0; i<n; i++)
+    {
+        s1 = 0, s2 = 0;
+        
+        for(j=0; j<n; j++)
+        {
+            s1 = s1 + G[i][j];
+            s2 = s2 + G[j][i];
+        }
+        
+        printf("The Outdegree(%c) = %d\n",i+97,s1);
+        printf("The Indegree(%c) = %d\n",i+97,s2);
+        printf("\n");
+    }
+}
+
+void main()
+{
+    readgraph();
+    
+    in_outdegree();
+}
